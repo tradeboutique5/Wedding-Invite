1b487830719d089db7773dc799467fc2dc6b7bcb
tradeboutique5 <tradeboutique5@gmail.com>
2026-07-09 17:11:02 +0200

    Update StorySection.tsx

--- wedding-invite/src/components/sections/StorySection.tsx
+++ wedding-invite/src/components/sections/StorySection.tsx
@@ -36,7 +36,7 @@ export default function StorySection({ story }: Props) {
               boxShadow: "0 20px 60px rgba(0,0,0,0.15)",
               backgroundImage: `url(${story.image})`,
               backgroundSize: "160%",
-              backgroundPosition: "right center",
+              backgroundPosition: "100% center",
               backgroundRepeat: "no-repeat",
             }}
           />
