2375bfaf11c866f5ad5568a92a7ab948c5262a95
tradeboutique5 <tradeboutique5@gmail.com>
2026-07-09 10:34:53 +0200

    Update wedding.config.ts

--- wedding-invite/config/wedding.config.ts
+++ wedding-invite/config/wedding.config.ts
@@ -107,8 +107,7 @@ export const weddingConfig = {
     title: "Lista di Nozze",
     subtitle: "Il Regalo Perfetto",
     heading: "Viaggio di Nozze",
-    message: "Siamo infinitamente grati per avervi nella nostra vita. Il dono più prezioso è quello di poter condividere la nostra gioia con voi. Se desiderate farci un regalo, il nostro sogno più grande è realizzare il viaggio di nozze che abbiamo sempre desiderato: ogni vostro pensiero sarà un passo verso questa indimenticabile avventura.
-              Con tutto il nostro affetto, Alessandro & Valentina.,
+    message: "Siamo infinitamente grati per avervi nella nostra vita. Il dono più prezioso è quello di poter condividere la nostra gioia con voi. Se desiderate farci un regalo, il nostro sogno più grande è realizzare il viaggio di nozze che abbiamo sempre desiderato: ogni vostro pensiero sarà un passo verso questa indimenticabile avventura.Con tutto il nostro affetto, Alessandro & Valentina.,
     iban: {
       enabled: true,
       holder: "Alessandro Cerasi e Valentina Palumbo",
